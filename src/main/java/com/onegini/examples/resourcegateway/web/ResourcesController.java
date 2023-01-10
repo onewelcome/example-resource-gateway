f521ba3e6f71996c39e1bb5f856981e55c4181f7
Pawel Lewandowski <pawel.lewandowski@onewelcome.com>
2023-01-10 07:46:28 +0100

    Add POST /resources API endpoint

--- src/main/java/com/onegini/examples/resourcegateway/web/ResourcesController.java
+++ src/main/java/com/onegini/examples/resourcegateway/web/ResourcesController.java
@@ -4,15 +4,8 @@ import static com.onegini.examples.resourcegateway.service.ScopeValidationServic
 import static com.onegini.examples.resourcegateway.service.ScopeValidationService.SCOPE_READ;
 import static com.onegini.examples.resourcegateway.service.ScopeValidationService.SCOPE_WRITE;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 import static org.springframework.http.HttpStatus.OK;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.onegini.examples.resourcegateway.model.ApplicationDetails;
 import com.onegini.examples.resourcegateway.model.DecoratedUser;
@@ -29,6 +22,19 @@ import com.onegini.examples.resourcegateway.service.tokenintrospection.TokenIntr
 import com.onegini.examples.resourcegateway.util.DecoratedUserIdBuilder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 @Slf4j
 @RestController
@@ -52,18 +58,21 @@ public class ResourcesController {
   }
 
   @GetMapping(value = "/application-details")
-  public ResponseEntity<ApplicationDetails> getApplicationDetails(@RequestHeader(name = AUTHORIZATION, required = false) final String authorizationHeader) {
+  public ResponseEntity<ApplicationDetails> getApplicationDetails(
+      @RequestHeader(name = AUTHORIZATION, required = false) final String authorizationHeader) {
     final TokenIntrospectionResult tokenIntrospectionResult = getTokenIntrospectionResultFromHeader(authorizationHeader);
     validateScopeAndTokenType(tokenIntrospectionResult, SCOPE_APPLICATION_DETAILS);
 
-    final ApplicationDetails applicationDetails = new ApplicationDetails(tokenIntrospectionResult.getAppIdentifier(), tokenIntrospectionResult.getAppPlatform(),
-        tokenIntrospectionResult.getAppVersion());
+    final ApplicationDetails applicationDetails =
+        new ApplicationDetails(tokenIntrospectionResult.getAppIdentifier(), tokenIntrospectionResult.getAppPlatform(),
+            tokenIntrospectionResult.getAppVersion());
 
     return new ResponseEntity<>(applicationDetails, OK);
   }
 
   @GetMapping(value = "/user-id-decorated")
-  public ResponseEntity<DecoratedUser> getDecoratedUserId(@RequestHeader(name = AUTHORIZATION, required = false) final String authorizationHeader) {
+  public ResponseEntity<DecoratedUser> getDecoratedUserId(
+      @RequestHeader(name = AUTHORIZATION, required = false) final String authorizationHeader) {
     final TokenIntrospectionResult tokenIntrospectionResult = getTokenIntrospectionResultFromHeader(authorizationHeader);
 
     tokenTypeValidationService.validateImplicitAuthenticationToken(tokenIntrospectionResult.getAmr());
@@ -76,14 +85,34 @@ public class ResourcesController {
   }
 
   @PostMapping(value = "/file-upload")
-  public ResponseEntity<MultipartResponse> fileUpload(@RequestHeader(name = AUTHORIZATION, required = false) final String authorizationHeader,
-                                                      @ModelAttribute final FormDataWithFiles formDataWithFiles) {
+  public ResponseEntity<MultipartResponse> fileUpload(
+      @RequestHeader(name = AUTHORIZATION, required = false) final String authorizationHeader,
+      @ModelAttribute final FormDataWithFiles formDataWithFiles) {
     final TokenIntrospectionResult tokenIntrospectionResult = getTokenIntrospectionResultFromHeader(authorizationHeader);
     validateScopeAndTokenType(tokenIntrospectionResult, SCOPE_WRITE);
 
     final MultipartResponse multipartResponse = multipartService.parse(formDataWithFiles);
 
     return new ResponseEntity<>(multipartResponse, OK);
+  }
+
+  @PostMapping
+  public void postResource(@RequestHeader(name = AUTHORIZATION, required = false) final String authorizationHeader,
+                           final HttpServletRequest request, final HttpServletResponse response) throws IOException {
+    final TokenIntrospectionResult tokenIntrospectionResult = getTokenIntrospectionResultFromHeader(authorizationHeader);
+    validateScopeAndTokenType(tokenIntrospectionResult, SCOPE_WRITE);
+    response.setStatus(HttpServletResponse.SC_CREATED);
+    response.setHeader(CONTENT_TYPE, request.getHeader(CONTENT_TYPE));
+
+    byte[] buffer = new byte[1024];
+    try (
+        InputStream input = request.getInputStream();
+        OutputStream output = response.getOutputStream()) {
+      for (int length = 0; (length = input.read(buffer)) > 0; ) {
+        output.write(buffer, 0, length);
+      }
+    }
+    response.flushBuffer();
   }
 
   private void validateScopeAndTokenType(final TokenIntrospectionResult tokenIntrospectionResult, final String requiredScope) {
