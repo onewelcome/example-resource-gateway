520885e2f9185c5d6d748a60a18f8f9aced59272
Pawel Lewandowski <pawel.lewandowski.it@gmail.com>
2025-06-25 15:31:47 +0200

    AMRs are not only represented by token types

--- src/main/java/com/onegini/examples/resourcegateway/service/TokenTypeValidationService.java
+++ src/main/java/com/onegini/examples/resourcegateway/service/TokenTypeValidationService.java
@@ -7,29 +7,28 @@ import java.util.Collection;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
-import com.onegini.examples.resourcegateway.model.TokenType;
 import com.onegini.examples.resourcegateway.model.exception.InvalidAccessTokenException;
 
 @Service
 public class TokenTypeValidationService {
 
-  public void validateImplicitAuthenticationToken(final Collection<TokenType> tokenTypes) {
-    if (isNoImplicitAuthenticationToken(tokenTypes)) {
+  public void validateImplicitAuthenticationToken(final Collection<String> amrs) {
+    if (isNoImplicitAuthenticationToken(amrs)) {
       throw new InvalidAccessTokenException("Token is not an implicit authentication access token");
     }
   }
 
-  public void validateNoImplicitAuthenticationToken(final Collection<TokenType> tokenTypes) {
-    if (isImplicitAuthenticationToken(tokenTypes)) {
+  public void validateNoImplicitAuthenticationToken(final Collection<String> amrs) {
+    if (isImplicitAuthenticationToken(amrs)) {
       throw new InvalidAccessTokenException("Token is an implicit authentication access token");
     }
   }
 
-  private boolean isImplicitAuthenticationToken(final Collection<TokenType> tokenTypes) {
-    return CollectionUtils.containsInstance(tokenTypes, IMPLICIT_AUTHENTICATION);
+  private boolean isImplicitAuthenticationToken(final Collection<String> amrs) {
+    return CollectionUtils.containsInstance(amrs, IMPLICIT_AUTHENTICATION.name());
   }
 
-  private boolean isNoImplicitAuthenticationToken(final Collection<TokenType> tokenTypes) {
+  private boolean isNoImplicitAuthenticationToken(final Collection<String> tokenTypes) {
     return !isImplicitAuthenticationToken(tokenTypes);
   }
 }
